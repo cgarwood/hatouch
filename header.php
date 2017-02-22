3cc06d4e06d4a5ce305fe0016b190bc875140557
Charles Garwood <cgarwood@gmail.com>
2017-02-21 19:42:50 -0500

    Updating config.js with more information. Updating 'user info' box in header

--- header.php
+++ header.php
@@ -99,7 +99,7 @@
 				<!-- The user image in the navbar-->
 				<!-- <img src="dist/img/user2-160x160.jpg" class="user-image" alt="User Image"> -->
 				<!-- hidden-xs hides the username on small devices so only the image appears. -->
-				<span class="hidden-xs">Living Room</span>
+				<span class="hidden-xs">{{config['location']}}</span>
 				</a>
 				<ul class="dropdown-menu">
 				<!-- The user image in the menu -->
@@ -107,32 +107,14 @@
 					<img src="dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
 
 					<p>
-					your name here
-					<small>Member since Nov. 2012</small>
+					{{config['location']}}
+					<small>Connected to HomeAssistant<br>{{ha_url}}</small>
 					</p>
-				</li>
-				<!-- Menu Body -->
-				<li class="user-body">
-					<div class="row">
-					<div class="col-xs-4 text-center">
-						<a href="#">Followers</a>
-					</div>
-					<div class="col-xs-4 text-center">
-						<a href="#">Sales</a>
-					</div>
-					<div class="col-xs-4 text-center">
-						<a href="#">Friends</a>
-					</div>
-					</div>
-					<!-- /.row -->
 				</li>
 				<!-- Menu Footer-->
 				<li class="user-footer">
-					<div class="pull-left">
-					<a href="#" class="btn btn-default btn-flat">Profile</a>
-					</div>
 					<div class="pull-right">
-					<a href="#" class="btn btn-default btn-flat">Sign out</a>
+					<a href="#" class="btn btn-default btn-flat">Lock Screen</a>
 					</div>
 				</li>
 				</ul>
