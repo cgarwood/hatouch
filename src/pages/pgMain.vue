94d071c9767948c6773c86a949fa718b721e0964
Charles Garwood <cgarwood@gmail.com>
2017-07-14 20:51:33 -0400

    Sizing updates

--- src/pages/pgMain.vue
+++ src/pages/pgMain.vue
@@ -1,7 +1,7 @@
 <template>
 <section class="content" v-cloak>
 	<div class="row">
-		<div class="col-md-6">
+		<div class="col-sm-6">
 			<div class="row">
 				<div class="col-sm-6">
 					<sensor entity_id="sensor.hallway_thermostat_temperature" title="Indoor Temperature" icon="fa-home" color-class="bg-red"></sensor>
@@ -19,17 +19,17 @@
 				</div>
 			</div>
 		</div>
-		<div class="col-md-6">
+		<div class="col-sm-6">
 			<div v-if="entities['sensor.pws_weather']" class="box box-widget widget-user">
 				<!-- Add the bg color to the header using any of the bg-* classes -->
-				<div class="widget-user-header bg-black" style="background: url('images/fountain.jpg') center center; background-size: cover; height: 108px;">
+				<div class="widget-user-header bg-black" style="background: url('images/fountain.jpg') center center; background-size: cover; height: 92px;">
 				<h3 class="widget-user-username" style="font-weight: bold;">Current Conditions</h3>
 				<h5 class="widget-user-desc">Home</h5>
 				</div>
-				<div class="widget-user-image" style="top: 50px;">
+				<div class="widget-user-image" style="top: 45px;">
 				<img class="img-circle" :src="entities['sensor.pws_weather']['attributes']['entity_picture']" :alt="entities['sensor.pws_weather']['state']" style="background-color:white;">
 				</div>
-				<div class="box-footer" style="padding-top: 20px;">
+				<div class="box-footer" style="padding-top: 15px;">
 					<div class="row">
 						<div class="col-sm-4 border-right">
 						<div class="description-block">
@@ -73,7 +73,7 @@
 						</div>
 						<div class="box-body">
 						<div class="chart">
-							<canvas id="areaChart" style="height: 128px;" height="128"></canvas>
+							<canvas id="areaChart" style="height: 108px;" height="108"></canvas>
 						</div>
 						</div>
 					</div>
@@ -81,7 +81,7 @@
 			</div>
 		</div>
 		<div class="col-sm-6">
-			<action title="Baby's Bedtime" icon="fa-bed" description="Dims living room lights, turns on cabinet lights. Turns off other lights" entity_id="scene.baby_bedtime"></action>
+			<action title="Baby's Bedtime" icon="fa-bed" description="Dims living room lights, turns on cabinet lights." entity_id="scene.baby_bedtime"></action>
 			<action title="Goodbye" icon="fa-automobile" description="Turns on porch light for 3 minutes (after dark)." entity_id="script.goodbye_lights"></action>
 			<action title="Goodnight" icon="fa-bed" description="Turns off all lights. Turns on dim cabinet lights." entity_id="script.goodnight"></action>
 		</div>
